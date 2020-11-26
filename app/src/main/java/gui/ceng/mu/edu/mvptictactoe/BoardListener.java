d7707b236d9042c64c4e7ee38e75cd49650496c2
U170709061 <fatmakaradag@posta.mu.edu.tr>
2020-11-26 23:08:39 +0300

    Initial commit

---
+++ app/src/main/java/gui/ceng/mu/edu/mvptictactoe/BoardListener.java
@@ -0,0 +1,9 @@
+package gui.ceng.mu.edu.mvptictactoe;
+
+public interface BoardListener {
+    byte NO_ONE = 0;
+    byte PLAYER_1 = 1;
+    byte PLAYER_2 = 2;
+    void playedAt(byte player, byte row, byte col);
+    void gameEnded(byte winner);
+}
